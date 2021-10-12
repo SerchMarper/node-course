a9f4af136461692f568e67d100c80422f05221e1
Sergio Martin <samp10@hotmail.com>
2021-10-12 18:50:42 -0500

    adding the new issues

---
+++ issueexcercises1/issue1-3.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+let userScore = 0;
+let conGame = true;
+let contVar = true;
+
+function promptAnswer(promptQuestion) {
+    return new Promise(resolve => {
+        let result = prompt(promptQuestion);
+        console.log("result:" + result);
+          resolve(result);
+      });
+    }
+
+async function asyncCall(asyncQuestion) {
+    console.log('calling');
+    const result = await promptAnswer(asyncQuestion);
+    console.log("Result:" + result);
+    return result;
+    // expected output: "resolved"
+  }
+
+function continueGame(){ 
+    var userNum = Number(promptAnswer("Choose a number between 1 and 6: "));
+    console.log("UserNum: " + userNum);
+  var randomNumber = Math.floor(Math.random() * 6) + 1;
+  if (typeof userNum === "number" && userNum >= 1 && userNum <= 6) {
+    if (userNum == randomNumber){
+      userScore += 2;
+    } else if ((userNum - randomNumber) ** 2 === 1){
+      userScore++;
+    }
+  }else {
+    console.log("You didn't entered a number between 1 and 6");
+  }
+  console.log(`User score: ${userScore}`);
+};
+
+continueGame();
+
+while(conGame == true){
+contVar = promptAnswer("Wanna play again? y/n: ");
+console.log("ContVar: " + contVar);
+if(contVar == "y"){
+  continueGame();
+  } else{
+  conGame = false;
+  console.log("Game ended.")
+  }
+}
---
+++ issueexcercises1/practice.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+function promptCall(){
+
+let result = prompt("Input a number between 1 and 6: ");
+return result;
+};
+
+console.log(promptCall());
