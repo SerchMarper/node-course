d8f2e2ca02745b1bd69043a0bf80fa5bbcf2508f
Sergio Martin <samp10@hotmail.com>
2021-11-09 16:02:16 -0500

    added destructuring, short circuiting and spread operator code exercises

--- destructuring-arrays/spread-operator.js
+++ destructuring-arrays/spread-operator.js
@@ -1,3 +1,39 @@
+'use strict';
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+
+    order: function(starterIndex, mainIndex){
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}){
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`);
+    },
+
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+    }
+};
+
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -16,3 +52,37 @@ const mainMenuCopy = [...restaurant.mainMenu];
 //Join 2 arrays
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 console.log(menu);
+
+//Iterable: arrays, string, maps, sets. NOT objects
+const str = 'Jonas';
+const letters = [...str, ' ', 's.'];
+console.log(letters);
+console.log(...str);
+// console.log(`s${...str} Schmedtmann`);
+
+//real world example
+const ingredients = [
+    'Tomato',
+    'Meat',
+    'Pasta'
+    // prompt(`Let's make pasta! Ingredient1?`),
+    // prompt(`Ingredient 2?`),
+    // prompt(`Ingredient 3`),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+
+//Objects
+const newRestaurant = {
+    foundedIn: 1998, 
+    ...restaurant,
+    founder: 'Giuseppe'    
+};
+console.log(newRestaurant);
+
+const restaurantCopy = {...restaurant};
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
